94a94eba8f5171a2b237364f18fcc506184b0bc0
Keala Lusk <contactkeala@gmail.com>
2015-08-15 11:15:49 -0700

    Added a link to reset button, identified css selectors for initial state

--- racing.js
+++ racing.js
@@ -1,2 +1,34 @@
+var player1, player2, counter, table1, table2
+//set an initial state at beginning
+//and with each reset
+
+player1 = 0
+player2 = 0
+
+table1 = 8
+table2 = 8
+
+$player1_initial_state = $("table tr td").eq(0)
+$player2_initial_state = $("table tr td").eq(9)
+
+if (player1 < table1.length || player2 < table1.length){
+  if ($(on("key press")) === a){
+    player1++
+  } else if ($(on("key press")) === l){
+    player2++
+  } else {
+  	return "'Tis nothing but a tie"
+  }
+}
+
+if (player1 === ){
+  alert("Player One Wins!")
+} else if (player2 reaches initial_state.length){
+  alert("Player Two Wins!")
+} else {
+  alert("You guys are both awesome! Tie :)")
+}
+
+for (var i = 0; i < 9)
 $(document).ready(function() {
-	$("").on("")
+  $("").on("")
